da5de40ce9ad3648cb0db92b0b5ba3ec4fa927de
Ian Cardinoza <ijcardin@ucalgary.ca>
2020-02-27 15:55:46 -0700

    Updates

---
+++ precache-manifest.d50f18b2638822b69cf1ba39a61ebabf.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "87f1987571444b589bd83c1fb8a5ef87",
+    "url": "/ianC/index.html"
+  },
+  {
+    "revision": "89228464b2d7153b9a9a",
+    "url": "/ianC/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "34d65fa402900bddf3f3",
+    "url": "/ianC/static/js/2.ea711598.chunk.js"
+  },
+  {
+    "revision": "5356fa2f66e46e6c05e4cbe319ac7f1d",
+    "url": "/ianC/static/js/2.ea711598.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "89228464b2d7153b9a9a",
+    "url": "/ianC/static/js/main.b1a22d8b.chunk.js"
+  },
+  {
+    "revision": "4aa98307448d05a746ff",
+    "url": "/ianC/static/js/runtime-main.cbe1ac5c.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/ianC/static/media/logo.5d5d9eef.svg"
+  }
+]);
